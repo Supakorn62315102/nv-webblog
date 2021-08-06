f934c774a1dcb0dec271ed2d21a037b7febbb6e4
Supakorn62315102 <supakornj62@nu.ac.th>
2021-08-06 22:00:11 +0700

    new

---
+++ server/server/src/app.js
@@ -0,0 +1,31 @@
+let express = require('express')
+let bodyParser = require('body-parser')
+let cors = require('cors')
+const {sequelize} = require('./models')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
+
+require('./routes')(app)
+
+app.get('/status', function (req, res ){
+  res.send('Hello nodejs server')
+})
+
+app.get('/hello/:person', function (req,res) {
+  console.log('hello - ' + req.params.person)
+  res.send('sey hello with ' + req.params.person)
+})
+
+
+let port = 8081
+
+sequelize.sync({force: false}).then(() => {
+  app.listen(port, function () {
+      console.log('Server running on ' + port)
+  })
+})
+
